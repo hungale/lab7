473357f3a08633b93e1abd7b788dc99987944782
hungale <zenithparadox@gmail.com>
2016-02-20 15:42:46 +0000

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -21,23 +21,4 @@ function likeClick(e) {
     e.preventDefault();
 
   ga("send", "event", 'like', 'click');
-  /*
-  // In an event listener, $(this) is 
-  // the element that fired the event
-  var projectTitle = $(this).find("p").text();
-
-  //"#" refers to id, "." refers to class 
-  var jumbotronHeader = $(".jumbotron h1");
-  jumbotronHeader.text(projectTitle);
-
-  var containingProject = $(this).closest(".project")
-  var description = $(containingProject).find(".project-description");
-
-  if(description.length == 0){
-    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
-  }
-  else{
-    description.fadeToggle()
-  }
-  */
 }
